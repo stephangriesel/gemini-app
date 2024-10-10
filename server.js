80f3d6ef5ba070d10db794a7f140161ed85c1869
Stephan Griesel <steph@ngriesel.com>
2024-10-10 06:56:43 +0200

    test backend

---
+++ server.js
@@ -0,0 +1,13 @@
+const PORT = 8000;
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.use(express.json());
+require('dotenv').config();
+
+const {GoogleGenerativeAI} = require('@google/generative-ai');
+
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
